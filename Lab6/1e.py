280d87967583aea815a635a57eb5e1ab06104f63
Ritwij Sinha <ritwijxsinha@gmail.com>
2017-09-06 14:37:21 +0530

    Add files via upload

--- Lab6/1e.py
+++ Lab6/1e.py
@@ -2,5 +2,5 @@ import re
 a = raw_input("Enter a string: ")
 b = raw_input("Enter the string to search: ")
 c = raw_input("Enter the string to replace: ")
-a = re.sub(b,c,a.rstrip())
+a = re.sub(b,c,a)
 print a
